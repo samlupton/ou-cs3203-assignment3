4106e03d9f044d9722c7d6b7b6f37018d1d773ea
moo20222 <samuel@ou.edu>
2023-09-13 08:56:41 -0500

    Initial Commit

---
+++ ou-cs3203-assignment3/ou_cs3203_assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  ou_cs3203_assignment3App.swift
+//  ou-cs3203-assignment3
+//
+//  Created by Samuel Lupton on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ou_cs3203_assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
